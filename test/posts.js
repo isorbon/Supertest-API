36f2a17b98b2d67f8e696f9e3eca9702fcf654ed
sorbon.imomnazarov <sorbon.imomnazarov@accenture.com>
2022-12-17 23:27:06 +0100

    Adding Negative API Tests

--- test/posts.js
+++ test/posts.js
@@ -22,7 +22,7 @@ describe.only("User Posts", () => {
       body: "My Blog Text Body",
     };
 
-    const postRes = await request 
+    const postRes = await request
       .post("posts")
       .set("Authorization", `Bearer ${TOKEN}`)
       .send(data);
@@ -40,38 +40,36 @@ describe.only("User Posts", () => {
   });
 
   // Negative Tests
-  describe('Negative Tests', () => {
-    it('401 Authentication Failed', async() => {
+  describe("Negative Tests", () => {
+    it("401 Authentication Failed", async () => {
       const data = {
         user_id: userId,
         title: "My Title",
         body: "My Blog Text Body",
       };
-  
-      const postRes = await request 
-        .post("posts")
-        .send(data);
-  
+
+      const postRes = await request.post("posts").send(data);
+
       //console.log(postRes);
       expect(postRes.statusCode).to.eq(401);
-      expect(postRes.body.message).to.eq('Authentication failed');
+      expect(postRes.body.message).to.eq("Authentication failed");
     });
 
-    it.only('422 Validation Failed', async() => {
+    it.only("422 Validation Failed", async () => {
       const data = {
         user_id: userId,
-        title: "My Title"
+        title: "My Title",
       };
-  
-      const postRes = await request 
+
+      const postRes = await request
         .post("posts")
         .set("Authorization", `Bearer ${TOKEN}`)
         .send(data);
-  
+
       // console.log(postRes.body);
       expect(postRes.statusCode).to.eq(422);
-      expect(postRes.body[0].field).to.eq('body');
+      expect(postRes.body[0].field).to.eq("body");
       expect(postRes.body[0].message).to.eq("can't be blank");
     });
-  })
+  });
 });
